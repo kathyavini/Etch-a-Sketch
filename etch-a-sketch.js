1141335f0fdd5edb10125288013ce84f35e293a4
kathyavini <kathyavini@gmail.com>
2021-11-14 14:12:25 -0800

    Add shading mode

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,9 @@
 const grid = document.querySelector('.grid');
 let currentColorTheme;
 let pointerEntryModeActive = true;
+let shadingMode = false;
 let pointerActiveBeforeErase;
+let shadingActiveBeforeErase;
 
 // Add event listeners to grid-layout buttons
 const grid9 = document.querySelector('.grid9');
@@ -52,6 +54,16 @@ bgSwitch.addEventListener('change', () => {
 	changeLayout();
 });
 
+// Add event listener for shading button
+const shading = document.querySelector('.shading');
+shading.addEventListener('click', () => {
+    toggleShadingMode();
+});
+
+function toggleShadingMode() {
+    shading.classList.toggle('active');
+    shadingMode = !shadingMode;
+}
 
 
 /*----------- ERASER FUNCTIONS --------*/
@@ -80,6 +92,9 @@ window.addEventListener("keydown", (ev) => {
         case 'e':
             toggleEraserMode();
             break;
+        case 's':
+            toggleShadingMode();
+            break;
     }
 });
 
@@ -107,6 +122,10 @@ function toggleEraserMode() {
             // turn it back off
             togglePointerEvents();
         }
+        if (shadingActiveBeforeErase) {
+            // turn it back on
+            toggleShadingMode();
+        }
     } else {
         // Turn on pointer events when you turn on eraser
         if (!pointerEntryModeActive) {
@@ -115,6 +134,13 @@ function toggleEraserMode() {
             pointerActiveBeforeErase = false;
         } else {
             pointerActiveBeforeErase = true;
+        }
+        // Turn off shading to return indices to 0
+        if (shadingMode) {
+            toggleShadingMode();
+            shadingActiveBeforeErase = true;
+        } else {
+            shadingActiveBeforeErase = false;
         }
         setColorArray('erase');
         eraser.classList.add('on');
@@ -127,7 +153,10 @@ function eraseOnDoubleTap() {
     grid.addEventListener("touchstart", (ev) => {
         currentTouch = ev.timeStamp;
         if (currentTouch - lastTouch <= 300) {
-            document.elementFromPoint(ev.touches[0].clientX, ev.touches[0].clientY).style.backgroundColor = '';
+            currentDiv = document.elementFromPoint(ev.touches[0].clientX, 
+                ev.touches[0].clientY);
+            currentDiv.style.backgroundColor = '';
+            currentDiv.index = 0;
         } else {
             lastTouch = currentTouch;
         }
@@ -141,13 +170,13 @@ function setColorArray(palette) {
     while (document.querySelector('.selected')) {
         document.querySelector('.selected').classList.remove('selected');
     }
-    // Turn off eraser
+    // Remove CSS indicator that eraser is on
     if (document.querySelector('.on')) {
         eraser.classList.remove('on');
     }
     switch(palette) {
         case 'autumn':
-            colorArray = ["#03071e","#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307","#ffba08"];
+            colorArray = ["#03071e","#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307","#ffba08"].reverse();
             autumn.classList.add('selected');
             break;
         case 'july':
@@ -155,7 +184,7 @@ function setColorArray(palette) {
             july.classList.add('selected');
             break;
         case 'purple':
-            colorArray = ["#10002b","#240046","#3c096c","#5a189a","#7b2cbf","#9d4edd","#c77dff","#e0aaff"];
+            colorArray = ["#10002b","#240046","#3c096c","#5a189a","#7b2cbf","#9d4edd","#c77dff","#e0aaff"].reverse();
             purple.classList.add('selected');
             break;
         case 'green':
@@ -167,7 +196,7 @@ function setColorArray(palette) {
             blue.classList.add('selected');
             break;
         case 'brown-tan':
-            colorArray = ["#1a120e","#33261f","#4b3b30","#645140","#7b6751","#937e62","#a89475","#bcaa87","#d4c197","#ebd8a7"];
+            colorArray = ["#1a120e","#33261f","#4b3b30","#645140","#7b6751","#937e62","#a89475","#bcaa87","#d4c197","#ebd8a7"].reverse();
             brownTan.classList.add('selected');
             break;
         case 'erase':
@@ -186,6 +215,7 @@ function createGrid(n) {
     for (let i = 1; i <= n*n; i++) {
         let div = document.createElement('div');
         // div.classList.add(`div${i}`); // Nice for troubleshooting
+        div.index = 0; // for shading mode
         div.classList.add(`generatedDiv`)
         grid.appendChild(div);
     }
@@ -199,11 +229,10 @@ function createGrid(n) {
 function colorChangeOnPointerEnter() {
     const targets = document.querySelectorAll('.grid div');
     targets.forEach((target) => {
-        // target.addEventListener('pointerenter', function() {
-        //     target.style.backgroundColor = pickColor();
-        // }, true);
         target.addEventListener('pointerenter', function() {
-            target.style.backgroundColor = pickColor();
+            newColorAndIndex = pickColor(target.index);
+            target.style.backgroundColor = newColorAndIndex[0];
+            target.index = newColorAndIndex[1];
         });
     });
 }
@@ -214,18 +243,30 @@ function colorChangeOnTouch() {
     grid.addEventListener("touchmove", (ev) => {
         touchedDiv = document.elementFromPoint(ev.touches[0].clientX, 
                 ev.touches[0].clientY);
-            if (touchedDiv && touchedDiv.classList.contains('generatedDiv') && 
+                if (touchedDiv && touchedDiv.classList.contains('generatedDiv') && 
                 !(touchedDiv === lastDiv)) {
-            touchedDiv.style.backgroundColor = pickColor();
-            lastDiv = touchedDiv;
-        }
+                    newColorAndIndex = pickColor(touchedDiv.index);
+                    touchedDiv.style.backgroundColor = newColorAndIndex[0];
+                    touchedDiv.index = newColorAndIndex[1];
+                    lastDiv = touchedDiv;
+                }
     });
 }
 
-function pickColor() {
-    // Select a random color from the colorArray
-    let color = colorArray[Math.floor(Math.random()*colorArray.length)];
-    return color;
+function pickColor(index) {
+    // Step through the palette if in shading mode
+    if (shadingMode) {
+        index = Math.min(index, colorArray.length - 1);
+        console.log(index);
+        let color = colorArray[index];
+        index++;
+        return [color, index];
+    }
+    // Otherwise select a random color from the colorArray
+    index = Math.floor(Math.random()*colorArray.length);
+    let color = colorArray[index];
+    // but div's index should return to the lightest color for next shading mode
+    return [color, 0];
 }
 
 function clearGrid() {
